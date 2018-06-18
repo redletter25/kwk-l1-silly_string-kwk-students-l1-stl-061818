e69a9037e9ede1a06491c8dd718407769e58bd78
redletter25 <maia.dothager@yahoo.com>
2018-06-18 21:18:13 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -57,7 +57,7 @@
 #puts "supercalifragilisticexpialidocious".length
 #puts "wow + mom"
 #puts "i really like programming".capitalize
-puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around ath"
+puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens "
 
 
 
